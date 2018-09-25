30ea350e5976a9638890712853527e3efabcffeb
Peter Pult <peter.pult@netlight.com>
2018-09-25 15:33:35 +0200

    Add npm and test for getHtml.

---
+++ test/getHtmlTest.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const assert = require('chai').assert;
+const getHtml = require('../lib/getHtml.js');
+
+describe('getHtml.handler', () => {
+  it('should get raw html code from a given url', (done) => {
+    let request = {
+      body: '{"sourceUrl":"http://example.org"}',
+      httpMethod: 'POST'
+    };
+    getHtml.handler(request, undefined, (error, response) => {
+      assert.equal(200, response.statusCode);
+      assert.include(response.body, 'Example Domain');
+      done();
+    });
+  });
+});
