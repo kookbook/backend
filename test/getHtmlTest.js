3ea7df74d683877b48ed7ed0c7153e97985a5bae
Peter Pult <peter.pult@netlight.com>
2018-09-27 10:58:31 +0200

    Beautify test.

--- test/getHtmlTest.js
+++ test/getHtmlTest.js
@@ -5,14 +5,15 @@ const getHtml = require('../lib/getHtml.js');
 
 describe('getHtml.handler', () => {
   it('should get raw html code from a given url', (done) => {
+    let sourceUrl = 'http://example.org';
     let request = {
-      body: '{"sourceUrl":"http://example.org"}',
+      body: `{"sourceUrl":"${sourceUrl}"}`,
       httpMethod: 'POST'
     };
     getHtml.handler(request, undefined, (error, response) => {
       assert.equal(200, response.statusCode);
       assert.include(response.body.html, 'Example Domain');
-      assert.equal('http://example.org', response.body.sourceUrl);
+      assert.equal(sourceUrl, response.body.sourceUrl);
       done();
     });
   });
--- test/parseHtmlTest.js
+++ test/parseHtmlTest.js
@@ -5,7 +5,7 @@ const parseHtml = require('../lib/parseHtml.js');
 const fs = require('fs');
 
 describe('parseHtml.handler', () => {
-  it('should parse raw html code to a recipe object', (done) => {
+  it('should parse raw html code to a recipe object from chefkoch.de', (done) => {
     let body = fs.readFileSync('test/chefkochBlob', 'utf8');
     let request = {
       body: body,
